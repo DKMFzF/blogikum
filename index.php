8766e56348c65b0950b707f17a3a5e7c44ece210
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-03 16:23:39 +0700

    InitCommit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru-RU">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="icon" href="./img/favicon.ico" size="any">
+  <link rel="icon" href="./img/favicon.svg" type="image/svg+xml" />
+  <link rel="apple-touch-icon" href="./img/apple-touch-icon.png" />
+  <meta name="discription" content="Блог товаров написанный на PHP" />
+  <link rel="stylesheet" href="./vendor/normalize.css">
+  <link rel="stylesheet" href="./vendor/bootstrap.min.css">
+  <title>PHP Blog</title>
+</head>
+<body>
+  
+</body>
+</html>
